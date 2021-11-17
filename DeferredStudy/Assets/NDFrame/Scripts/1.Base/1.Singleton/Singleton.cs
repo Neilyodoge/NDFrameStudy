a42dfa983ec6d483c9b3e420068e27440646aa71
Neilyodog <1838233556@qq.com>
2021-11-17 22:00:47 +0800

    框架学习11.17
    单例基类

---
+++ DeferredStudy/Assets/NDFrame/Scripts/1.Base/1.Singleton/Singleton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 单例模式的基类，全局只有一个
+/// </summary>
+public class Singleton<T> where T : new()
+{
+    private static T instance;
+    public static T Instance
+    {
+        get 
+        {
+            if (instance == null)   // 没有被实例化过
+            {
+                instance = new T();
+            }
+            return instance;
+        }
+    }
+}
--- DeferredStudy/Assets/NDFrame/Test/Test.cs
+++ DeferredStudy/Assets/NDFrame/Test/Test.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Test1 : Singleton<Test1>
+{
+    public string text = "222";
+}
 public class Test : MonoBehaviour
 {
     void Start()
     {
-
-    }
-    void Update()
-    {
-        
+        Debug.Log(Test1.Instance.text);
     }
 }
