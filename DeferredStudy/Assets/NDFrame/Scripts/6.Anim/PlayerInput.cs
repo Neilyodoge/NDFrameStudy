1b5fe7fda99e5df847f20af30065917eed7369cd
王浩宇 <1838233556@qq.com>
2022-03-30 10:14:58 +0800

    AnimL2-5

--- DeferredStudy/Assets/NDFrame/Scripts/6.Anim/PlayerInput.cs
+++ DeferredStudy/Assets/NDFrame/Scripts/6.Anim/PlayerInput.cs
@@ -12,6 +12,16 @@ public class PlayerInput : MonoBehaviour
 
     public float Dup;
     public float Dright;
+    public float DirBlendSpeed = 0.1f;
+    public bool inputEnable = true;     // 关掉这个模块
+
+    private float targetDup;            // 平滑前分辨方向的参数
+    private float targetDright;
+    private float velocityDup;          // 会自动算，无需参数
+    private float velocityDright;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +31,15 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Dup = (Input.GetKey(KeyUp) ? 1.0f : 0) - (Input.GetKey(KeyDown) ? 1.0f : 0);
-        Dright = (Input.GetKey(KeyRight) ? 1.0f : 0) - (Input.GetKey(KeyLeft) ? 1.0f : 0);
+        targetDup = (Input.GetKey(KeyUp) ? 1.0f : 0) - (Input.GetKey(KeyDown) ? 1.0f : 0);          // ()?():()
+        targetDright = (Input.GetKey(KeyRight) ? 1.0f : 0) - (Input.GetKey(KeyLeft) ? 1.0f : 0);
+        if (inputEnable == false)
+        {
+            targetDup = 0;
+            targetDright = 0;
+        }
+        // 平滑
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, DirBlendSpeed);
+        Dup = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, DirBlendSpeed);
     }
 }
